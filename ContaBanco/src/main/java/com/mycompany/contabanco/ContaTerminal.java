994beff41abe2759043eb2475a7c4b50a3557a60
MiragemS <musantos016@gmail.com>
2025-02-07 17:50:21 -0300

    complementação

--- ContaBanco/src/main/java/com/mycompany/contabanco/ContaTerminal.java
+++ ContaBanco/src/main/java/com/mycompany/contabanco/ContaTerminal.java
@@ -2,9 +2,16 @@ package com.mycompany.contabanco;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Murilo Santos 
+ * @version 1.1
+ * 
+ */
 public class ContaTerminal {
-
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         int numero;
         String agencia, nomeCliente;
